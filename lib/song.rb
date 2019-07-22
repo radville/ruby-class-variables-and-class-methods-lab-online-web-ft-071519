24aceea1b659ee9fb4285903c67e570671f2dd06
Laura Radville <laura.radville@gmail.com>
2019-07-22 20:14:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,22 @@
 class Song
   attr_accessor :name, :artist, :genre
-  
+  @@count = 0
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres
   end
-  
-  @@count = 0
   
   def self.count
     @@count
   end
   
-  @@genres = []
-  
+  def self.genres
+    @@genres
+  end
 
 end
